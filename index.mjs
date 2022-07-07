1444eb7b124b4f224b381a66fb3213d4a6bcf223
omar elsahragty <omar@affable.ai>
2022-07-07 21:01:30 +0200

    [EDIT] lib

--- index.mjs
+++ index.mjs
@@ -1,15 +1,15 @@
 import { createServer } from "net";
-import gt06n from "./gt06n.mjs";
+import gt06n from "./lib/gt06n.mjs";
 
 createServer((connection) => {
   connection.setEncoding("hex");
   connection.on("data", (packet) => {
-    const parsedData = gt06n.parse(packet);
+    const parsedData = gt06n(packet);
 
     console.log(parsedData);
 
-    if (parsedData.event === "login") {
-      connection.write(Buffer.from("787805010001d9dc0d0a", "hex"));
+    if (parsedData.responseMessage) {
+      connection.write(parsedData.responseMessage);
     }
   });
 })
