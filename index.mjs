de731f1c7b9f96850d455def4cfd195551259b61
omar elsahragty <omar@affable.ai>
2022-07-08 04:33:36 +0200

    [EDIT] error handling

--- index.mjs
+++ index.mjs
@@ -3,19 +3,24 @@ import gt06n from "./lib/gt06n.mjs";
 
 createServer((connection) => {
   connection.setEncoding("hex");
-  connection.on("data", (packet) => {
-    console.log("PACKET", packet);
+  connection
+    .on("data", (packet) => {
+      console.log("PACKET", packet);
 
-    const parsedPacket = gt06n(packet);
+      const parsedPacket = gt06n(packet);
 
-    console.log("PARSED PACKET", parsedPacket);
+      console.log("PARSED PACKET", parsedPacket);
 
-    if (!parsedPacket) return;
+      if (!parsedPacket) return;
 
-    if (parsedPacket.responseMessage) {
-      connection.write(parsedPacket.responseMessage);
-    }
-  });
+      if (parsedPacket.responseMessage) {
+        connection.write(parsedPacket.responseMessage);
+      }
+    })
+    .on("error", (err) => {
+      console.error("error", err);
+      connection.end();
+    });
 })
   .on("listening", () => console.log("server is running on port 3333"))
   .listen(3333);
